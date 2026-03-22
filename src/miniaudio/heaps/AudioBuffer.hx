cf88904609dcf1da3575ea07ba5eafd9fec6fe92
dtwotwo <dprojetmail@gmail.com>
2026-03-22 20:25:36 +0300

    remove unused legacy heaps integration

--- src/miniaudio/heaps/AudioBuffer.hx
+++ src/miniaudio/heaps/AudioBuffer.hx
@@ -1,52 +1,17 @@
 package miniaudio.heaps;
 
 import haxe.io.Path;
-import hxd.fs.FileEntry;
 
 class AudioBuffer extends hxd.res.Sound
 {
-	var cachedData:Null<hxd.snd.Data>;
+	private var buffer:Null<Miniaudio.Buffer>;
 
-	public function new(entry:FileEntry)
+	public function getBuffer():Miniaudio.Buffer
 	{
-		super(entry);
-		entry.watch(() -> cachedData = null);
-	}
+		buffer ??= Miniaudio.Buffer.fromBytes(entry.getBytes());
+		if (buffer == null)
+			throw '${entry.path}: ${Miniaudio.describeLastError()}';
 
-	override public function getData():hxd.snd.Data
-	{
-		if (cachedData != null)
-			return cachedData;
-
-		final bytes = entry.getBytes();
-		final ext = Path.extension(entry.path).toLowerCase();
-
-		cachedData = switch (ext)
-		{
-			case 'ogg', 'opus', 'flac', 'wav', 'mp3', 'aiff':
-				loadWithMiniaudio(bytes);
-			default:
-				super.getData();
-		};
-
-		if (cachedData == null)
-			throw '${entry.path}: ${miniaudio.Miniaudio.describeLastError()}';
-
-		return cachedData;
-	}
-
-	override public function dispose()
-	{
-		stop();
-		cachedData = null;
-	}
-
-	static function loadWithMiniaudio(bytes:haxe.io.Bytes):hxd.snd.Data
-	{
-		final decoded = miniaudio.Miniaudio.decodeToPCMFloat(bytes);
-		if (decoded == null)
-			return null;
-
-		return new AudioData(decoded.bytes, decoded.channels, decoded.sampleRate, decoded.samples, decoded.floatFormat);
+		return buffer;
 	}
 }
